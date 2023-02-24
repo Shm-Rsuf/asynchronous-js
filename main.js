b757be96cd986c9be0d8ec324388cc9f88627b5d
shm_usuf <shmusuf@gmail.com>
2023-02-24 19:29:57 +0600

    promise added

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const wrapper = document.querySelector('.country-wrapper');
+const wrapper = document.querySelector(".country-wrapper");
 /****************asynchronous javascript******************/
 const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v2/name/argentina');
+request.open("GET", "https://restcountries.com/v2/name/argentina");
 request.send();
 
 // console.log(request.responseText);
 
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText)
+request.addEventListener("load", function () {
+  const [data] = JSON.parse(this.responseText);
   renderData(data);
-})
+});
 
 function renderData(country) {
-  console.log(country);
+  // console.log(country);
 
   const html = `
       <div class="country">
@@ -21,5 +21,27 @@ function renderData(country) {
         <p>${country.capital}</p>
         <p>${(country.population / 1000000).toFixed(2)} M people</p>
       </div>`;
-  wrapper.insertAdjacentHTML('afterbegin', html);
+  wrapper.insertAdjacentHTML("afterbegin", html);
 }
+
+//promise in javascript
+console.log("welcome");
+//create promise
+const promise = new Promise((resolve, reject) => {
+  let isPromise = true;
+  if (isPromise) {
+    resolve("Promise completed");
+  } else {
+    reject("Not completed");
+  }
+});
+
+promise
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+console.log("end");
